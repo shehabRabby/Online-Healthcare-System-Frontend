caee6a4bd0c16a3cb5ad7091620157386e65784c
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2026-09-20 19:38:33 +0600

    OTP validation running

--- src/hooks/auth.hooks.ts
+++ src/hooks/auth.hooks.ts
@@ -1,4 +1,4 @@
-import { getMe, googleOAuth, userLogin, userLogout, userRegistration } from "@/api";
+import { getMe, googleOAuth, userLogin, userLogout, userRegistration, verifyAccount } from "@/api";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export function useLogin() {
@@ -6,6 +6,13 @@ export function useLogin() {
     mutationFn: userLogin,
   });
 }
+
+export function useVerifyAccount() {
+  return useMutation({
+    mutationFn: verifyAccount,
+  });
+}
+
 
 export function useRegistration() {
   return useMutation({
